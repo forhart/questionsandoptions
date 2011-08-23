f26187f0c09cd806537995a728cf668a4b7579ef
Foras <foraart@gmail.com>
2011-08-23 12:58:24 +0530

    Wrote method for unicode representation of the models. __unicode__()

--- simplelist/models.py
+++ simplelist/models.py
@@ -12,11 +12,15 @@ class Question(models.Model):
     title = models.CharField(max_length=300)
     asked_date = models.DateField()
 
+    def __unicode__(self):
+        return self.title
+
 class Option(models.Model):
     text  = models.CharField(max_length=300)
     question = models.ForeignKey(Question)
 
-
+    def __unicode__(self):
+        return self.text
 
 admin.site.register(Question)
 admin.site.register(Option)
