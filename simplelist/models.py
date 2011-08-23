6f97c17aedf3d7d5f3c04ecdb5ee36051a709a46
Foras <foraart@gmail.com>
2011-08-23 12:40:42 +0530

    Questions and Options models created.

--- simplelist/models.py
+++ simplelist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=300)
+    asked_date = models.DateField()
+
+class Option(models.Model):
+    text  = models.CharField(max_length=300)
+    question = models.ForeignKey(Question)
